1dc29d760366669425b32b91d4bb3566492af64c
wenchao <iwenchaos6688@163.com>
2023-12-29 10:04:11 +0800

    添加log工具类

--- app/src/main/java/com/chaos/mediafun/SimplePlayerActivity.kt
+++ app/src/main/java/com/chaos/mediafun/SimplePlayerActivity.kt
@@ -2,10 +2,13 @@ package com.chaos.mediafun
 
 import android.os.Environment
 import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import com.chaos.mediafun.base.BaseActivity
 import com.chaos.mediafun.databinding.ActivitySimplePlayerBinding
 import com.chaos.mediafun.media.decoder.AudioDecoder
 import com.chaos.mediafun.media.decoder.VideoDecoder
+import com.chaos.mediafun.utils.LogUtils
 import java.util.concurrent.Executors
 
 /**
@@ -24,6 +27,34 @@ class SimplePlayerActivity : BaseActivity<ActivitySimplePlayerBinding>() {
         initPlayer()
     }
 
+    private fun initPlayer() {
+        val threadPool = Executors.newFixedThreadPool(10)
+        videoDecoder = VideoDecoder(filePath, binding.surfaceView, null)
+        threadPool.execute(videoDecoder)
+
+        audioDecoder = AudioDecoder(filePath)
+        threadPool.execute(audioDecoder)
+        videoDecoder.goOn()
+        audioDecoder.goOn()
+    }
+
+    private fun initListener(){
+        binding.seekBar.setOnSeekBarChangeListener(object :OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                LogUtils.i("TAG","seekbar progress:${progress}")
+//                videoDecoder.seekTo()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+    }
+
+
     fun playOrPause(view:View) {
         if(videoDecoder.isDecoding()){
             videoDecoder.pause()
@@ -35,7 +66,6 @@ class SimplePlayerActivity : BaseActivity<ActivitySimplePlayerBinding>() {
 
     }
 
-
     fun clickRepack(view: View) {
         repack()
     }
@@ -43,17 +73,6 @@ class SimplePlayerActivity : BaseActivity<ActivitySimplePlayerBinding>() {
     private fun repack() {
 //        val repack = MP4Repack(path)
 //        repack.start()
-    }
-
-    private fun initPlayer() {
-        val threadPool = Executors.newFixedThreadPool(10)
-        videoDecoder = VideoDecoder(filePath, binding.surfaceView, null)
-        threadPool.execute(videoDecoder)
-
-        audioDecoder = AudioDecoder(filePath)
-        threadPool.execute(audioDecoder)
-        videoDecoder.goOn()
-        audioDecoder.goOn()
     }
 
     override fun onDestroy() {
